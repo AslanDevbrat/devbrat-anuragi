f661ee0c9f913e7e7a148631f88549b974f68ef0
AslanDevbrat <a.devbrat@yahoo.com>
2026-07-12 12:25:41 +0200

    Added Eurohaptic poster

--- src/App.tsx
+++ src/App.tsx
@@ -400,6 +400,7 @@ function App() {
               subtitle="Proc. EuroHaptics 2026 Conference Papers"
               date="2026"
               description={["D. Anuragi, A. Wu, A. Farooq, M. Nayak, R. Singh, P. Coe, M. Salmimaa, and R. Raisamo", "A related demo paper is also accepted and will be presented at EuroHaptics 2026."]}
+              poster="https://canva.link/advewj8u8cdi88k"
               image={`${import.meta.env.BASE_URL}Pictures/eurohaptic.png`}
             />
             <ListEntry
